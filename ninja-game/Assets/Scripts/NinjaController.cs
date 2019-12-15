f282b26b95b9cc427f468ab3bd7437e50fddabd4
linha9 <linha9theawesome@gmail.com>
2019-12-15 17:28:55 -0300

    Refactoring on ninja's controller

--- ninja-game/Assets/Scripts/NinjaController.cs
+++ ninja-game/Assets/Scripts/NinjaController.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class NinjaController : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 10.0f;
+    private float XSpeed = 200.0f;
     [SerializeField]
-    private float movementSmooth = 0.0f;
+    private float XSpeedRun = 300.0f;
+    [SerializeField]
+    private float XAcceleration = 3.0f;
+    [SerializeField]
+    private float XMoveSmooth = 0.0f;
 
     private Rigidbody2D rb2d;
     private Animator animator;
 
-    private Vector2 velocity;
+    private Vector2 currentVelocity;
     private bool isFacingRight;
     private float horizontalInput;
+
+    private float currentSpeed;
 
     #region Unity Cycle
     private void Awake()
@@ -22,14 +28,50 @@ public class NinjaController : MonoBehaviour
         this.rb2d = this.GetComponent<Rigidbody2D>();
         this.animator = this.GetComponent<Animator>();
         this.isFacingRight = true;
-        this.velocity = Vector2.zero;
+        this.currentVelocity = Vector2.zero;
+        this.currentSpeed = this.XSpeed;
     }
 
     private void Update()
     {
+        CaptureHorizontalMovementInput();
+    }
+
+    private void CaptureHorizontalMovementInput()
+    {
         this.horizontalInput = Input.GetAxis("Horizontal");
+
+        CaptureRunInput();
+        CaptureFlipNecessity();
+
         animator.SetFloat("Speed", Mathf.Abs(this.horizontalInput));
     }
+
+    private void CaptureRunInput()
+    {
+        if (Input.GetButton("Run") && this.currentSpeed < this.XSpeedRun)
+        {
+            this.currentSpeed += this.XAcceleration;
+        }
+        else if (Input.GetButtonUp("Run"))
+        {
+            this.currentSpeed = this.XSpeed;
+        }
+    }
+
+    private void CaptureFlipNecessity()
+    {
+        if (this.horizontalInput < 0.0f && this.isFacingRight)
+        {
+            Flip();
+        }
+        else if (this.horizontalInput > 0.0f && !this.isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+
 
     private void FixedUpdate()
     {
@@ -42,17 +84,8 @@ public class NinjaController : MonoBehaviour
 
     private void Move()
     {
-        Vector2 targetLocation = new Vector2(this.speed * this.horizontalInput * Time.deltaTime, rb2d.velocity.y);
-        this.rb2d.velocity = Vector2.SmoothDamp(this.rb2d.velocity, targetLocation, ref this.velocity, this.movementSmooth);
-
-        if (this.horizontalInput < 0.0f && this.isFacingRight)
-        {
-            Flip();
-        }
-        else if (this.horizontalInput > 0.0f && !this.isFacingRight)
-        {
-            Flip();
-        }
+        Vector2 targetLocation = new Vector2(this.currentSpeed * this.horizontalInput * Time.deltaTime, rb2d.velocity.y);
+        this.rb2d.velocity = Vector2.SmoothDamp(this.rb2d.velocity, targetLocation, ref this.currentVelocity, this.XMoveSmooth);
     }
 
     private void Flip()
